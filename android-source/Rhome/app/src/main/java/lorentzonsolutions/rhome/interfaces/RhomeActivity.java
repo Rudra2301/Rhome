95404404af0af571c942676215a392174191c6f3
Johan Lorentzon <lorentzon@live.se>
2017-05-25 13:32:56 +0200

    Interface for this applications activities

---
+++ android-source/Rhome/app/src/main/java/lorentzonsolutions/rhome/interfaces/RhomeActivity.java
@@ -0,0 +1,10 @@
+package lorentzonsolutions.rhome.interfaces;
+
+/**
+ * Created by johanlorentzon on 2017-05-25.
+ */
+
+public interface RhomeActivity {
+    void initEvents();
+    void assignViews();
+}
